c6e49fda9c8ac9334637a4198a10e35c8d6c6853
Ekachcka <2331286yan@gmail.com>
2022-01-31 20:12:35 +0200

    Add 4

--- index.js
+++ index.js
@@ -1,78 +1,78 @@
 let index;
 let sum;
 
-// // 4)
+// 4)
 
-// for (index = false; index ===false;) {
-//     sum=+prompt('for:2+2*2 = ')
-//     if (sum===6) {
-//         index=true;
-//     }
+for (index = false; index ===false;) {
+    sum=+prompt('for:2+2*2 = ')
+    if (sum===6) {
+        index=true;
+    }
+}
+index=false;
+sum=NaN;
+while (index===false) {
+    sum=+prompt('while:2+2*2 = ')
+    if (sum===6) {
+        index=true;
+    }
+}
+index=false;
+sum=NaN;
+do {
+    sum=+prompt('doWhile:2+2*2 = ')
+    if (sum===6) {
+        index=true;
+    }
+}while (index===false);
+
+// // 1)
+
+// for (index = 25; index >=0; index--) {
+//     console.log(index);
 // }
-// index=false;
-// sum=NaN;
-// while (index===false) {
-//     sum=+prompt('while:2+2*2 = ')
-//     if (sum===6) {
-//         index=true;
-//     }
+// index=25;
+// while (index>=0) {
+//     console.log(index--);
 // }
-// index=false;
-// sum=NaN;
+// index=25;
 // do {
-//     sum=+prompt('doWhile:2+2*2 = ')
-//     if (sum===6) {
-//         index=true;
-//     }
-// }while (index===false);
+//     console.log(index--);
+// }while (index>=0);
 
-// 1)
-
-for (index = 25; index >=0; index--) {
-    console.log(index);
-}
-index=25;
-while (index>=0) {
-    console.log(index--);
-}
-index=25;
-do {
-    console.log(index--);
-}while (index>=0);
-
-// 2)
+// // 2)
 
 
-for(index=10;index<=50;index+=5){
-    console.log(index);
-}
-index=10;
-while (index<50) {
-    console.log(index);
-    index+=5;
-}
-index=10;
-do {
-    console.log(index);
-    index+=5;
-}while (index<=50);
+// for(index=10;index<=50;index+=5){
+//     console.log(index);
+// }
+// index=10;
+// while (index<50) {
+//     console.log(index);
+//     index+=5;
+// }
+// index=10;
+// do {
+//     console.log(index);
+//     index+=5;
+// }while (index<=50);
 
-// 3)
+// // 3)
 
-for ( index = 2,sum=1; index <= 100; index++) {
-    sum+=index;
-}
-console.log(sum);
-index=2;
-sum=1;
-while (index<=100) {
-    sum+=index++;
-}
-console.log(sum);
-index=2;
-sum=1;
-do {
-    sum+=index++;
-}while (index<=100);
-console.log(sum);
+// for ( index = 2,sum=1; index <= 100; index++) {
+//     sum+=index;
+// }
+// console.log(sum);
+// index=2;
+// sum=1;
+// while (index<=100) {
+//     sum+=index++;
+// }
+// console.log(sum);
+// index=2;
+// sum=1;
+// do {
+//     sum+=index++;
+// }while (index<=100);
+// console.log(sum);
 
